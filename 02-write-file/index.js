44be5e53cb9652dff7f1cc7fc415676af2a453c7
maksimizer <maximorlov@mail.ru>
2022-05-21 15:42:22 +0300

    feat: solve 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdout, stdin, exit } = process;
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Please, type some text.\n');
+stdin.on('data', data => {
+  if (data.toString().includes('exit')) {
+      exit();
+  } else {
+    output.write(data.toString());
+  };
+});
+process.on('exit', () => stdout.write('Have a nice day!'));
+process.on('SIGINT', exit);
