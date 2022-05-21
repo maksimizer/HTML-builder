1a8a5a383200c361cbc62e7c47a02aefe2ebf855
maksimizer <maximorlov@mail.ru>
2022-05-21 21:04:09 +0300

    feat: solve 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+async function getFilesInfo(dirPath) {
+  try {
+    const data  = await fs.promises.readdir(dirPath, {withFileTypes: true});
+    data.forEach(file => {
+      if (file.isFile()) {
+        fs.stat(path.join(dirPath, file.name), (error, stats) => {
+          if (error) {
+            console.log(error.message);
+          } else {
+            const filePath = path.join(dirPath, file.name);
+            console.log(`${path.parse(filePath).name} - ${path.extname(filePath).slice(1)} - ${stats.size}b`);
+          };
+        });
+      };
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+getFilesInfo(dirPath);
